898ff06f2400d87f5dcf9e1c9e0e99995f1914c2
AKASH-PRASAD7 <akash.csel016@bppimt.ac.in>
2023-10-16 16:20:08 +0530

    fixed typo

--- client/src/query/client.js
+++ client/src/query/client.js
@@ -4,14 +4,15 @@ const URL = import.meta.env.VITE_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 const httpLink = createHttpLink({
-  uri: URL,
+  uri: "https://shoptrend-grafbase-api-akash-prasad7.grafbase.app/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      "x-api-key": API_KEY,
+      "x-api-key":
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTIxMTM4MzUsImlzcyI6ImdyYWZiYXNlIiwiYXVkIjoiMDFIN1dYRzRWUkdSUzkwRzRZWlRYTkUzUkQiLCJqdGkiOiIwMUg3V1hHNTZBRzZGOURNTjUzQThERzRHRyIsImVudiI6InByb2R1Y3Rpb24iLCJwdXJwb3NlIjoicHJvamVjdC1hcGkta2V5In0.h6bsH6ehOXjl6ampHU06cv009rx0y_Gcj4Xv_hQtH1s",
     },
   };
 });
